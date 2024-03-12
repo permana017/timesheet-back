5ef0c52bfe944e73556aa22250686270b6153574
permana017 <ahdipermana07@gmail.com>
2024-03-12 23:01:35 +0700

    add env

--- helper/connection.js
+++ helper/connection.js
@@ -1,14 +1,15 @@
 const { Client } = require("pg");
-// require('dotenv').config()
+require("dotenv").config();
 
-// const { DB_USERNAME, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env
+const { DB_USERNAME, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env;
 
 const db = new Client({
-  user: "postgres.etlcpfodkmsiwkdvouqr",
-  host: "aws-0-ap-southeast-1.pooler.supabase.com",
-  database: "postgres",
-  password: "Supabase@123",
-  port: "5432",
+  user: DB_USERNAME,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  rs,
 });
 
 db.connect((err) => {
